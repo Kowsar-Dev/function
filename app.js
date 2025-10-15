190d651bffab5d41b68e7683d14c0b5e28bb0e5f
Kowsar-Dev <kowsara276@gmail.com>
2025-10-15 11:34:44 +0600

    new change

--- app.js
+++ app.js
@@ -1,389 +1,453 @@
-//----- Function -----//
+// //----- Function -----//
 
-// let firstName = "Kowsar";
-// let lastName = "Ahmed";
-// let result = greetUser(firstName,lastName)
-// function greetUser (firstName ,lastName){
+// // let firstName = "Kowsar";
+// // let lastName = "Ahmed";
+// // let result = greetUser(firstName,lastName)
+// // function greetUser (firstName ,lastName){
     
-//     let fullName = firstName+lastName;
-//     return fullName;
+// //     let fullName = firstName+lastName;
+// //     return fullName;
 
+// // }
+
+// // console.log(result);
+
+
+
+// // function outer(a){
+// //   function inner(b){
+// //     let z = a+b;
+// //     return z;
+// //   }
+// //   return inner;
+// // }
+
+// // let outerResult = outer(2)
+
+// // console.log(outerResult(4));
+
+// //1
+
+// let name = "Kowsar";
+
+// function greetUser (name){
+//     let nam = name;
+//      return nam;
+    
 // }
 
-// console.log(result);
+// console.log("Welcome " ,greetUser(name));
 
 
+// //2
 
-// function outer(a){
-//   function inner(b){
-//     let z = a+b;
-//     return z;
-//   }
-//   return inner;
+// let sum = 0;
+
+// function sumMath (a,b){
+//    sum = (a+b);
+//    return sum;
 // }
-
-// let outerResult = outer(2)
-
-// console.log(outerResult(4));
-
-//1
-
-let name = "Kowsar";
-
-function greetUser (name){
-    let nam = name;
-     return nam;
-    
-}
-
-console.log("Welcome " ,greetUser(name));
+// console.log(sumMath(4,6));
 
 
-//2
-
-let sum = 0;
-
-function sumMath (a,b){
-   sum = (a+b);
-   return sum;
-}
-console.log(sumMath(4,6));
+// //3
 
 
-//3
+// let result = 0;
 
-
-let result = 0;
-
-let doubleNumber =  ((num1) => {
-    result = num1*2 ;
+// let doubleNumber =  ((num1) => {
+//     result = num1*2 ;
   
-     return result;
-    }
-)
-let number1 = doubleNumber(4);
-let number2 = doubleNumber(6)
-console.log(number1,number2);
+//      return result;
+//     }
+// )
+// let number1 = doubleNumber(4);
+// let number2 = doubleNumber(6)
+// console.log(number1,number2);
 
 
 
-//4
+// //4
 
-function dinnerCost (price ,texRate = 0.05){
-    return price + (price*texRate);
+// function dinnerCost (price ,texRate = 0.05){
+//     return price + (price*texRate);
 
-}
-let cost = dinnerCost(100);
-console.log(cost)
-
-
-//5
-
-let stringLength = (string =>string.length)
-let string = stringLength("i love myself");
-console.log(string);
+// }
+// let cost = dinnerCost(100);
+// console.log(cost)
 
 
-//6
+// //5
 
-function  applyDiscount (price ,percentage){
-   return price-(price*(percentage/100))
-}
- function totalPrice(){
-    let total = applyDiscount(200,10);
-    return total;
-
-    }
-    console.log(totalPrice());
-
-//7
-
-let age = 1;
-
-function isAdult (age){
-    if(age>=18){
-        let isAdult = "You are Adult";
-        return isAdult;
-    }
-    else if(age<18 && age >=1){
-        let isChild ="You are a Child";
-        return isChild;
-    }
-    else{
-        return "Invalid input"
-    }
-}
-
-console.log(isAdult(age));
+// let stringLength = (string =>string.length)
+// let string = stringLength("i love myself");
+// console.log(string);
 
 
-//8
+// //6
 
-function randomNumber (){
-    let randomNumber = (Math.floor(Math.random() *10)+1);
-    return randomNumber;
+// function  applyDiscount (price ,percentage){
+//    return price-(price*(percentage/100))
+// }
+//  function totalPrice(){
+//     let total = applyDiscount(200,10);
+//     return total;
 
-}
-console.log(randomNumber());
+//     }
+//     console.log(totalPrice());
+
+// //7
+
+// let age = 1;
+
+// function isAdult (age){
+//     if(age>=18){
+//         let isAdult = "You are Adult";
+//         return isAdult;
+//     }
+//     else if(age<18 && age >=1){
+//         let isChild ="You are a Child";
+//         return isChild;
+//     }
+//     else{
+//         return "Invalid input"
+//     }
+// }
+
+// console.log(isAdult(age));
 
 
-//9
+// //8
 
-function boxVolume (length,width,height){
-    let volume =( length*width*height);
-    return volume;
+// function randomNumber (){
+//     let randomNumber = (Math.floor(Math.random() *10)+1);
+//     return randomNumber;
 
-
-}
-let volume = boxVolume (5,2,3);
-console.log(volume);
-
-//10
+// }
+// console.log(randomNumber());
 
 
-const tasks = ["Buy Milk","Code","Sleep"];
+// //9
 
-function printList (tasks){
-    let i;
-    tasks.forEach(task => 
-    { i = task;
-     console.log(i)}
+// function boxVolume (length,width,height){
+//     let volume =( length*width*height);
+//     return volume;
+
+
+// }
+// let volume = boxVolume (5,2,3);
+// console.log(volume);
+
+// //10
+
+
+// const tasks = ["Buy Milk","Code","Sleep"];
+
+// function printList (tasks){
+//     let i;
+//     tasks.forEach(task => 
+//     { i = task;
+//      console.log(i)}
    
-    );
+//     );
    
-}
-printList(tasks)
+// }
+// printList(tasks)
 
 
-//11
-let temp = 16;
-function checkTemp (temp){
-    if(temp>30){
-     return "It's Hot!"
-    }
-    else {
-        return "It's mild or cool"
-    }
-}
-let temperature = checkTemp (temp);
-console.log(temperature);
+// //11
+// let temp = 16;
+// function checkTemp (temp){
+//     if(temp>30){
+//      return "It's Hot!"
+//     }
+//     else {
+//         return "It's mild or cool"
+//     }
+// }
+// let temperature = checkTemp (temp);
+// console.log(temperature);
 
 
-//12
+// //12
 
-let base  = 5;
-let powerCalculator = (( base, exponent = 2 )=>{
-    return base**exponent
+// let base  = 5;
+// let powerCalculator = (( base, exponent = 2 )=>{
+//     return base**exponent
    
-})
-console.log(powerCalculator(base));
+// })
+// console.log(powerCalculator(base));
 
-//13
+// //13
 
-let content = "hello"
-function makeListItem (content){
-    return `<li> ${content} </li>`
+// let content = "hello"
+// function makeListItem (content){
+//     return `<li> ${content} </li>`
 
-}
-console.log(makeListItem(content));
+// }
+// console.log(makeListItem(content));
 
-//14
+// //14
 
-let num = 5;
-function isPositive (num){
-  if(num>0){
-    return "True"
-  }
-  else{
-    return "False"
-  }
-}
-console.log(isPositive(num));
+// let num = 5;
+// function isPositive (num){
+//   if(num>0){
+//     return "True"
+//   }
+//   else{
+//     return "False"
+//   }
+// }
+// console.log(isPositive(num));
 
-//15
+// //15
 
-let count = 6;
-function Counter (){
+// let count = 6;
+// function Counter (){
     
-    count= count+1;
-    return count 
-}
-Counter();
-console.log(Counter(count))
+//     count= count+1;
+//     return count 
+// }
+// Counter();
+// console.log(Counter(count))
 
-//16
+// //16
 
-function execute (callback){
-    callback();
+// function execute (callback){
+//     callback();
 
-}
-execute (()=>{
-    console.log("Task Done")
-})
+// }
+// execute (()=>{
+//     console.log("Task Done")
+// })
 
 
-//17
+// //17
 
-const color = ["Red","Blue","Green"];
+// const color = ["Red","Blue","Green"];
 
-function getFirstElement(color) {
-    return color [0];
+// function getFirstElement(color) {
+//     return color [0];
 
     
-}
-console.log(getFirstElement(color));
+// }
+// console.log(getFirstElement(color));
 
 
-//18
+// //18
 
-const user = {name :"Alice",age :24};
+// const user = {name :"Alice",age :24};
 
-function getUserAge(user) {
-    let age = user.age;
-    return age;
-}
-console.log(getUserAge(user));
+// function getUserAge(user) {
+//     let age = user.age;
+//     return age;
+// }
+// console.log(getUserAge(user));
 
-//19
-let stringName = "kowsar";
-function capitalizeName (stringName){
-    let capitalize = stringName.charAt(0).toUpperCase()+stringName.slice(1).toLowerCase();
-    return capitalize;
+// //19
+// let stringName = "kowsar";
+// function capitalizeName (stringName){
+//     let capitalize = stringName.charAt(0).toUpperCase()+stringName.slice(1).toLowerCase();
+//     return capitalize;
 
-}
-console.log(capitalizeName(stringName));
+// }
+// console.log(capitalizeName(stringName));
 
-//20
+// //20
 
 
-function outerFunction (){
-    let message = "Hello";
-    function innerFunction(){
-          return message;
-    }
-    let inner = innerFunction ;
-    return inner;
-}
+// function outerFunction (){
+//     let message = "Hello";
+//     function innerFunction(){
+//           return message;
+//     }
+//     let inner = innerFunction ;
+//     return inner;
+// }
 
-let call = outerFunction();
+// let call = outerFunction();
 
-console.log(call())
+// console.log(call())
 
-//21
+// //21
 
-let card = [];
+// let card = [];
 
-function shoppingCard (item){
-    card.push(item)
-}
+// function shoppingCard (item){
+//     card.push(item)
+// }
 
-shoppingCard("Apples1")
+// shoppingCard("Apples1")
 
-shoppingCard("Apples2")
-console.log(card);
+// shoppingCard("Apples2")
+// console.log(card);
 
-//22
+// //22
 
-const numbers = [10,20,30];
-function  removeNumber (num){
-    numbers.pop(num)  
-}
-removeNumber(num);
-console.log(numbers);
+// const numbers = [10,20,30];
+// function  removeNumber (num){
+//     numbers.pop(num)  
+// }
+// removeNumber(num);
+// console.log(numbers);
 
-//23
+// //23
 
-const queue = ["Anya","Ben","Chloe"];
+// const queue = ["Anya","Ben","Chloe"];
 
-function removeFront  (names){
-    queue.shift(names);
-}
-removeFront();
-console.log(queue)
+// function removeFront  (names){
+//     queue.shift(names);
+// }
+// removeFront();
+// console.log(queue)
 
-//24
+// //24
 
-const queue1 = ["Ben","Chloe"];
-let name1 = "VIP Customer"
-function addName (name1){
-    queue1.unshift(name1)
-}
-addName(name1);
-console.log(queue1);
+// const queue1 = ["Ben","Chloe"];
+// let name1 = "VIP Customer"
+// function addName (name1){
+//     queue1.unshift(name1)
+// }
+// addName(name1);
+// console.log(queue1);
 
-//25
+// //25
 
- const movies = ["Dune", "Arrival", "Interstellar"];
+//  const movies = ["Dune", "Arrival", "Interstellar"];
 
- function forOFLoop (){
-    let s = "";
-    for(let movie of movies){
-       s += movie +` `;
+//  function forOFLoop (){
+//     let s = "";
+//     for(let movie of movies){
+//        s += movie +` `;
        
-    }
-    return s ;
- }
+//     }
+//     return s ;
+//  }
 
-console.log(forOFLoop());
+// console.log(forOFLoop());
 
-//26
+// //26
 
-const inventory = { apples: 5, bananas: 10, oranges: 3 };
+// const inventory = { apples: 5, bananas: 10, oranges: 3 };
 
-function forInLoop (){
-    let s = "";
-    for(let inven in inventory){
-         s += inven + ` `;
-    }
-    return s;
-}
-console.log(forInLoop())
+// function forInLoop (){
+//     let s = "";
+//     for(let inven in inventory){
+//          s += inven + ` `;
+//     }
+//     return s;
+// }
+// console.log(forInLoop())
 
-//27
+// //27
 
-const inventory1 = { apples: 5, bananas: 10 };
+// const inventory1 = { apples: 5, bananas: 10 };
 
- function forInLoop1 (){
-    let s = "";
-    for(let inven in inventory1){
-         s += `${inventory1[inven]}\n`;
-    }
-    return s;
-}
-console.log(forInLoop1())
+//  function forInLoop1 (){
+//     let s = "";
+//     for(let inven in inventory1){
+//          s += `${inventory1[inven]}\n`;
+//     }
+//     return s;
+// }
+// console.log(forInLoop1())
 
-//28
+// //28
 
-const originalScores = [100, 90, 80];
+// const originalScores = [100, 90, 80];
 
-function saveGame (){
-   let newScores = originalScores.slice();
-   return newScores;
-}
-console.log(saveGame());
+// function saveGame (){
+//    let newScores = originalScores.slice();
+//    return newScores;
+// }
+// console.log(saveGame());
 
-//29
+// //29
 
-const allScores = [100, 90, 85,80, 70, 60];
+// const allScores = [100, 90, 85,80, 70, 60];
 
-function scores (){
-    let allScore = allScores.slice(0,3)
-    return allScore;
-}
-console.log(scores());
+// function scores (){
+//     let allScore = allScores.slice(0,3)
+//     return allScore;
+// }
+// console.log(scores());
 
 
-//30
+// //30
 
-const data = [5, 12, 3, 22, 8];
-const filtered = [];
-function filterList (){
-    for(let info of data){
-        if(info>10){
-            filtered.push(info)
-        }
-    }
-}
-filterList();
-console.log(filtered)
+// const data = [5, 12, 3, 22, 8];
+// const filtered = [];
+// function filterList (){
+//     for(let info of data){
+//         if(info>10){
+//             filtered.push(info)
+//         }
+//     }
+// }
+// filterList();
+// console.log(filtered)
+
+
+// const array = [1,2,3,4,5,6,7,8,9,10];
+
+// const newArray = array.filter(function (e){
+//           return e % 2 == 0;
+// })
+
+// console.log(newArray);
+
+
+
+
+// let user = {
+//     name : "Kowsar",
+//     email : "kowsara2766@gmail.com",
+//     phone : "01763449070" ,
+// }
+
+// function messageSend (msg , sendTo) {
+//     return ` ${msg} ,${sendTo}`
+    
+// }
+
+// let email = messageSend("Email send to :",user.email)
+// let sms = messageSend("Sms send to :",user.phone)
+// console.log(email);
+// console.log(sms);
+
+
+// let user1 = {
+//     name : "Kowsar Ahmed",
+//     email : "kowsara2766@gmail.com",
+//     phone : "01763449070" ,
+// }
+// let user2 = {
+//     name : "Mohsin mormo",
+//     email : "mohsinmormo@gmail.com",
+//     phone : "01522112387" ,
+// }
+
+// function smsSend(user) {
+//     console.log("Sms send to :",user.phone);
+// }
+
+
+// function msgSend(user) {
+//     console.log("Message send to :",user.phone);
+// }
+
+
+// function emailSend(user) {
+//     console.log("Email send to :",user.email);
+// }
+
+// function sendMessage (user ,notify) {
+//     console.log("Since you are authorized");
+//     notify(user)
+    
+// }
+
+// sendMessage(user1, emailSend);
+// sendMessage(user2, msgSend)
+
 
